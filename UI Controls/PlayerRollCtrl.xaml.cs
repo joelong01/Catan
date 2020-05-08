8d4e9cf169cc2b7951d4b122395bbacb06a83a9c
Joe Long <joelong@outlook.com>
2020-05-07 22:21:38 -0700

    rolling dice UI started

---
+++ UI Controls/PlayerRollCtrl.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Catan10
+{
+    public delegate void RolledDelegate(int roll);
+    public sealed partial class PlayerRollCtrl : UserControl
+    {
+        bool _rolled = false;
+        List<RollCtrl> _rollControls = new List<RollCtrl>();
+
+        public RolledDelegate OnRolled;
+        public PlayerRollCtrl()
+        {
+            this.InitializeComponent();
+            _rollControls.Add(RollCtrl_One);
+            _rollControls.Add(RollCtrl_Two);
+            _rollControls.Add(RollCtrl_Three);
+            _rollControls.Add(RollCtrl_Four);
+        }
+
+        private void Roll_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (_rolled) return;
+
+            RollCtrl rollCtrl = sender as RollCtrl;
+            _roll = rollCtrl.Roll;
+            rollCtrl.Orientation = TileOrientation.FaceUp;
+            OnRolled?.Invoke(_roll);
+        }
+
+        private int _roll = 0;
+        public int Roll => _roll;
+
+        public async Task Reset()
+        {
+            var list = new List<Task>();
+            _rollControls.ForEach((ctrl) => list.Add(ctrl.GetFlipTask(TileOrientation.FaceDown)));
+            await Task.WhenAll(list);
+            _rolled = false;
+
+        }
+
+        public void Randomize()
+        {
+            _rollControls.ForEach((ctrl) => ctrl.Randomize());
+        }
+        private bool clicked = false;
+        private async void OnShowAll(object sender, RoutedEventArgs e)
+        {
+            if (clicked) return;
+            clicked = true;
+
+            var list = new List<Task>();
+            _rollControls.ForEach((ctrl) => list.Add(ctrl.GetFlipTask(TileOrientation.FaceUp)));
+            await Task.WhenAll(list);
+            clicked = false;
+        }
+
+        private async void OnReset(object sender, RoutedEventArgs e)
+        {
+            await Reset();
+            Randomize();
+        }
+
+        private async void OnFaceUp(object sender, RoutedEventArgs e)
+        {
+            var list = new List<Task>();
+            _rollControls.ForEach((ctrl) => list.Add(ctrl.GetFlipTask(TileOrientation.FaceUp)));
+            await Task.WhenAll(list);
+        }
+
+        private async void OnFaceDown(object sender, RoutedEventArgs e)
+        {
+            var list = new List<Task>();
+            _rollControls.ForEach((ctrl) => list.Add(ctrl.GetFlipTask(TileOrientation.FaceDown)));
+            await Task.WhenAll(list);
+        }
+    }
+}
---
+++ UI Controls/RollCtrl.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Catan10
+{
+    public sealed partial class RollCtrl : UserControl
+    {
+        MersenneTwister twist = new MersenneTwister();
+
+        public RollCtrl()
+        {
+            this.InitializeComponent();
+            FlipClose.Begin();
+        }
+        public static readonly DependencyProperty DiceOneProperty = DependencyProperty.Register("DiceOne", typeof(string), typeof(RollCtrl), new PropertyMetadata("\U00012680"));
+        public string DiceOne
+        {
+            get => (string)GetValue(DiceOneProperty);
+            set => SetValue(DiceOneProperty, value);
+        }
+
+        public static readonly DependencyProperty DiceTwoProperty = DependencyProperty.Register("DiceTwo", typeof(string), typeof(RollCtrl), new PropertyMetadata("\U00012680"));
+        public string DiceTwo
+        {
+            get => (string)GetValue(DiceTwoProperty);
+            set => SetValue(DiceTwoProperty, value);
+        }
+        public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register("Orientation", typeof(TileOrientation), typeof(RollCtrl), new PropertyMetadata(TileOrientation.FaceDown, OrientationChanged));
+        public TileOrientation Orientation
+        {
+            get => (TileOrientation)GetValue(OrientationProperty);
+            set => SetValue(OrientationProperty, value);
+        }
+        private static void OrientationChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var depPropClass = d as RollCtrl;
+            var depPropValue = (TileOrientation)e.NewValue;
+            depPropClass?.SetOrientation(depPropValue);
+        }
+        private void SetOrientation(TileOrientation orientation)
+        {
+            if (orientation == TileOrientation.FaceDown) FlipClose.Begin();
+            if (orientation == TileOrientation.FaceUp) FlipOpen.Begin();
+
+            //SetupFlipAnimation(orientation, DoubleAnimation_FlipBackTile, DoubleAnimation_FlipFrontTile, MainPage.GetAnimationSpeed(AnimationSpeed.VeryFast), 0);
+            //StoryBoard_Flip.Begin();
+        }
+
+        private int _roll = 0;
+        public int Roll => _roll;
+        private string GetDiceString(int val)
+        {
+            string key = $"Dice_{val}";
+            return (string)App.Current.Resources[key];
+        }
+        public void Randomize()
+        {
+            int index = twist.Next(1, 6);
+            _roll = index;
+            DiceOne = GetDiceString(index);
+            index = twist.Next(1, 6);
+            _roll += index;
+            DiceTwo = GetDiceString(index);            
+        }
+
+        public Task GetFlipTask(TileOrientation orientation)
+        {
+            //var animationTime = SetupFlipAnimation(orientation, DoubleAnimation_FlipBackTile, DoubleAnimation_FlipFrontTile, MainPage.GetAnimationSpeed(AnimationSpeed.Normal), 0);
+            //StoryBoard_Flip.Duration = TimeSpan.FromMilliseconds(animationTime);
+            //return StoryBoard_Flip.ToTask();
+
+            if (orientation == TileOrientation.FaceDown) return FlipClose.ToTask();
+            if (orientation == TileOrientation.FaceUp) return FlipOpen.ToTask();
+            throw new InvalidEnumArgumentException();
+        }
+
+        private double SetupFlipAnimation(TileOrientation orientation, DoubleAnimation back, DoubleAnimation front, double animationTimeInMs, double startAfter = 0)
+        {
+
+            if (orientation == TileOrientation.FaceUp)
+            {
+                back.To = -90;
+                front.To = 0;
+                front.Duration = TimeSpan.FromMilliseconds(animationTimeInMs);
+                back.Duration = TimeSpan.FromMilliseconds(animationTimeInMs);
+                back.BeginTime = TimeSpan.FromMilliseconds(startAfter);
+                front.BeginTime = TimeSpan.FromMilliseconds(startAfter + animationTimeInMs);
+            }
+            else
+            {
+                back.To = 0;
+                front.To = 90;
+                back.Duration = TimeSpan.FromMilliseconds(animationTimeInMs);
+                front.Duration = TimeSpan.FromMilliseconds(animationTimeInMs);
+                front.BeginTime = TimeSpan.FromMilliseconds(startAfter);
+                back.BeginTime = TimeSpan.FromMilliseconds(startAfter + animationTimeInMs);
+
+            }
+            return animationTimeInMs;
+
+        }
+
+    }
+}
