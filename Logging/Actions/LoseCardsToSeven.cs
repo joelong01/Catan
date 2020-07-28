acdd24e2ce0ea54c8203bbb2a1c4d16462836b48
Joe Long <joelong@outlook.com>
2020-07-28 15:06:32 -0700

    in a multiplayer game, we weren't sending a message to move the baron from the machine that discarded the cards because of a 7 (that wasn't the current player)

--- Logging/Actions/LoseCardsToSeven.cs
+++ Logging/Actions/LoseCardsToSeven.cs
@@ -9,7 +9,7 @@ namespace Catan10
     /// </summary>
     public class LoseCardsToSeven : LogHeader, ILogController
     {
-        public TradeResources LostResources { get; set;  }
+        public TradeResources LostResources { get; set; }
         public static Task PostMessage(IGameController gameController, TradeResources lostCards)
         {
             Contract.Assert(lostCards.Count > 0); //don't negate them
@@ -26,25 +26,20 @@ namespace Catan10
             PlayerModel sentBy = gameController.NameToPlayer(this.SentBy);
             sentBy.GameData.Resources.GrantResources(LostResources.GetNegated());
             gameController.MainPageModel.Bank.GameData.Resources.GrantResources(LostResources);
-            if (gameController.CurrentPlayer == gameController.TheHuman)
+            foreach (var player in gameController.MainPageModel.PlayingPlayers)
             {
-                foreach (var player in gameController.MainPageModel.PlayingPlayers)
-                {
-                    //
-                    //  stop if any player has more than 7 cards
-                    //
-                    if (player.GameData.Resources.Current.Count > 7)
-                    {
-                        return Task.CompletedTask;
-                    }
-                }
-
                 //
-                //  no player has more than 7 cards -- move the baron!
-                return MustMoveBaronLog.PostLog(gameController, MoveBaronReason.Rolled7);
+                //  stop if any player has more than 7 cards - the last person to get rid of 1/2 there cards will send the message to move the baron
+                //
+                if (player.GameData.Resources.Current.Count > 7)
+                {
+                    return Task.CompletedTask;
+                }
             }
 
-            return Task.CompletedTask;
+            //
+            //  no player has more than 7 cards -- move the baron!
+            return MustMoveBaronLog.PostLog(gameController, MoveBaronReason.Rolled7);
         }
 
         public Task Redo(IGameController gameController)
@@ -52,7 +47,7 @@ namespace Catan10
             throw new NotImplementedException();
         }
 
-        public Task Replay (IGameController gameController)
+        public Task Replay(IGameController gameController)
         {
             PlayerModel sentBy = gameController.NameToPlayer(this.SentBy);
             sentBy.GameData.Resources.GrantResources(LostResources.GetNegated());
